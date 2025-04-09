29b8fb8c1e3b134e50cb5d6a48560840c16b0635
felipe brostolin <felipebrostolinribeiro@gmail.com>
2025-04-09 17:57:43 -0300

    Added custom cursor and intellij fix

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -12,3 +12,4 @@ vim.opt.inccommand = "split"
 vim.keymap.set("n", "grn", vim.lsp.buf.rename)
 vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
 vim.keymap.set("n", "grr", vim.lsp.buf.references)
+
