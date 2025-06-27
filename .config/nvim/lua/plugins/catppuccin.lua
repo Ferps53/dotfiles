32336a756bc7f207a10c69a9f4c885527c97b149
ferps <felipebrostolinribeiro@gmail.com>
2025-06-27 19:25:23 -0300

    fix: transparent terminal with catppuccin background

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,14 +1,14 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	opts = {
-		transparent_background = true,
-	},
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+  },
+  config = function()
+    vim.cmd.colorscheme("catppuccin")
     vim.cmd("highlight Normal guibg=none")
     vim.cmd("highlight NonText guibg=none")
-	end,
+  end,
 }
