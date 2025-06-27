7d46b18c2099552b611ab7ac303c4a043ae56ada
Felipe Brostolin Ribeiro <felipebrostolinribeiro@gmail.com>
2025-06-27 18:03:10 -0300

    better nvim and hypr

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -7,7 +7,7 @@ return {
 		transparent_background = true,
 	},
 	config = function()
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("catppuccin-macchiato")
     vim.cmd("highlight Normal guibg=none")
     vim.cmd("highlight NonText guibg=none")
 	end,
