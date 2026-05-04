345b0d731a629b700d14a0004f6eb1457793360b
Felipe Brostolin Ribeiro <felipebrostolinribeiro@gmail.com>
2026-05-04 19:32:54 -0300

    fix(nix): improve battery life

--- nixos/hosts/ferps-home/configuration.nix
+++ nixos/hosts/ferps-home/configuration.nix
@@ -10,7 +10,15 @@
 
   networking.hostName = "ferps-home";
 
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      libva-vdpau-driver
+      libvdpau-va-gl
+    ];
+  };
+
   services.xserver.videoDrivers = ["nvidia"];
   boot.kernelParams = ["nvidia_drm.modeset=1" "nvidia_drm.fbdev=1"];
 
@@ -18,9 +26,27 @@
     modesetting.enable = true;
     open = false;
     nvidiaSettings = true;
+    powerManagement = {
+      enable = true;
+    };
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  services.power-profiles-daemon.enable = false;
+
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
     GBM_BACKEND = "nvidia-drm";
--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -95,6 +95,8 @@
     package = pkgs.jdk21;
   };
 
+  programs.direnv.enable = true;
+
   zramSwap.enable = true;
   zramSwap.algorithm = "zstd";
   zramSwap.memoryPercent = 50; # Ele usará até 50% da sua RAM para criar o dispositivo comprimido
@@ -134,6 +136,8 @@
       brightnessctl
       bibata-cursors
       btop
+      direnv
+      jq
 
       # Ferramentas de Dev
       zig
@@ -142,6 +146,7 @@
       openssl
       prisma-engines
       lombok
+      claude-code
 
       # LSPs, Formatadores e Dependências do Neovim
       alejandra
@@ -162,12 +167,14 @@
 
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
 
       JAVA_8_HOME = "${pkgs.jdk8}/lib/openjdk";
       JAVA_11_HOME = "${pkgs.jdk11}/lib/openjdk";
       JAVA_17_HOME = "${pkgs.jdk17}/lib/openjdk";
       JAVA_21_HOME = "${pkgs.jdk21}/lib/openjdk";
       JAVA_25_HOME = "${pkgs.jdk25}/lib/openjdk";
+      LOMBOK_JAR = "${pkgs.lombok}/share/java/lombok.jar";
 
       PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
       PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
