b053fa54a9d86496e1c3561cf08f3e553b781286
Felipe Brostolin Ribeiro <felipebrostolinribeiro@gmail.com>
2026-05-28 21:33:16 -0300

    fix: voltar para o kernel 7.0.6

--- nixos/modules/common.nix
+++ nixos/modules/common.nix
@@ -39,7 +39,18 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackagesFor (
+      pkgs.linux_latest.override {
+        argsOverride = rec {
+          version = "7.0.6";
+          modDirVersion = "7.0.6";
+          src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+            sha256 = "08vm18wx6399phzgr3wz94yga3ab4fyca79445ygvbspm904996b";
+          };
+        };
+      }
+    );
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
